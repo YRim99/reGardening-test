ca07855f04ff88fe9b6bf6333d9bea719dc12057
sohee7528 <sohee7528@hanmail.net>
2021-09-14 14:34:42 +0900

    유리,고철,플라스틱,분리배출

---
+++ app/src/main/java/com/example/regardening_test/styrofoam.kt
@@ -0,0 +1,11 @@
+package com.example.regardening_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class styrofoam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_styrofoam)
+    }
+}
