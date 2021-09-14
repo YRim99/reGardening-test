0a22913a196dd877aa882967110e61247e9373ab
sohee7528 <sohee7528@hanmail.net>
2021-09-14 10:45:33 +0900

    비닐,플라스틱

---
+++ app/src/main/java/com/example/regardening_test/glass.kt
@@ -0,0 +1,11 @@
+package com.example.regardening_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class glass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_glass)
+    }
+}
