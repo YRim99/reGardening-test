624a4e061a186a49b863700ea2cdb03e19d33b00
YRim99 <alsrod2017@naver.com>
2021-09-14 17:01:32 +0900

    ㅁ

--- app/src/main/java/com/example/regardening_test/MainActivity.kt
+++ app/src/main/java/com/example/regardening_test/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.regardening_test
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var main_click : TextView
@@ -24,6 +26,21 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    fun imagemove(image1: ImageView, posix:Float, posiy: Float, duration1:Long){
+        runnable = object :Runnable{
+            override fun run() {
+                ObjectAnimator.onFloat(image1, "translationX", posix).apply{
+                    duration = duration1
+                    start()
+                }
 
-
+                ObjectAnimator.onFloat(image1, "translationY", posiy).apply{
+                    duration = duration1
+                    start()
+                }
+                handler.postDelayed(runnable,duration1)
+            }
+        }
+        handler.post(runnable)
+    }
 }
