db2b0ce60d47262e6e9ff04ed2a2358288635453
YRim99 <alsrod2017@naver.com>
2021-09-14 17:16:51 +0900

    q

--- app/src/main/java/com/example/regardening_test/MainActivity.kt
+++ app/src/main/java/com/example/regardening_test/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.regardening_test
 
-import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var main_click : TextView
     lateinit var imageView : ImageView
-    var handler : Handler = Handler()
-    var runnable : Runnable = Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,22 +21,5 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, Home::class.java)
             startActivity(intent)
         }
-    }
-    fun imagemove(image1: ImageView, posix:Float, posiy: Float, duration1:Long){
-        runnable = object :Runnable{
-            override fun run() {
-                ObjectAnimator.onFloat(image1, "translationX", posix).apply{
-                    duration = duration1
-                    start()
-                }
-
-                ObjectAnimator.onFloat(image1, "translationY", posiy).apply{
-                    duration = duration1
-                    start()
-                }
-                handler.postDelayed(runnable,duration1)
-            }
-        }
-        handler.post(runnable)
     }
 }
