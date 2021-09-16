034f19687ffdb0f8e8fed9a7f8a069c1dcea93aa
h33j <tnqhd4ever@naver.com>
2021-09-17 00:39:25 +0900

    tip_iron 찐 끝

--- app/src/main/java/com/example/regardening_test/separate.kt
+++ app/src/main/java/com/example/regardening_test/separate.kt
@@ -7,15 +7,20 @@ import android.widget.Button
 
 class separate : AppCompatActivity() {
     lateinit var btn_vinil : Button
-
+    lateinit var btn_can : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_separate)
         btn_vinil = findViewById<Button>(R.id.btn_vinil)
+        btn_can = findViewById(R.id.btn_can)
 
         btn_vinil.setOnClickListener{
             var intent = Intent(this, vinil::class.java)
+            startActivity(intent)
+        }
+        btn_can.setOnClickListener{
+            var intent = Intent(this, iron::class.java)
             startActivity(intent)
         }
     }
