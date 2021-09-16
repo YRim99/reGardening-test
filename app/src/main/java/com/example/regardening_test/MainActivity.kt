8efc2916032151d8a8f3ff6a8011d0b103ada959
YRim99 <alsrod2017@naver.com>
2021-09-16 09:49:39 +0900

    a

--- app/src/main/java/com/example/regardening_test/MainActivity.kt
+++ app/src/main/java/com/example/regardening_test/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.regardening_test
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var background : ImageView = findViewById(R.id.imageView)
+
+        ObjectAnimator.ofFloat(background, "translationX", 100f).apply {
+            duration = 2000
+            start()
+        }
+
         main_click = findViewById(R.id.main_click)
         main_click.setOnClickListener {
             var intent = Intent(this, Home::class.java)
             startActivity(intent)
         }
+
     }
 }
