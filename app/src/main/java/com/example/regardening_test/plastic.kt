67eba7bc4693cb062d6baad96c83c6a1decabc8b
h33j <tnqhd4ever@naver.com>
2021-09-17 01:38:31 +0900

    tip_plastic 끝

--- app/src/main/java/com/example/regardening_test/plastic.kt
+++ app/src/main/java/com/example/regardening_test/plastic.kt
@@ -1,11 +1,21 @@
 package com.example.regardening_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class plastic : AppCompatActivity() {
+    lateinit var back: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plastic)
+        back = findViewById<Button>(R.id.back)
+
+        back.setOnClickListener {
+            var intent = Intent(this, separate::class.java)
+            startActivity(intent)
+        }
     }
 }
