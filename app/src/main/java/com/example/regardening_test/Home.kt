c59d9347ae7d7fd99df8f08087c8f5053787bd8b
YRim99 <alsrod2017@naver.com>
2021-09-22 22:10:39 +0900

    녜림

--- app/src/main/java/com/example/regardening_test/Home.kt
+++ app/src/main/java/com/example/regardening_test/Home.kt
@@ -3,22 +3,29 @@ package com.example.regardening_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.example.regardening_test.databinding.ActivityHomeBinding
+import com.example.regardening_test.databinding.MissionDialogBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
     private val frag_garden = frag_garden()
     private val frag_collection = frag_collection()
-    private val frag_mission = frag_mission()
+    private val frag_tip = frag_tip()
     private val frag_history = frag_history()
+    private val frag_mission = frag_mission()
     private lateinit var bnv_home : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        //하단 메뉴 선택창
         bnv_home = findViewById(R.id.bnv_home)
-
         initNavigationBar()
     }
 
@@ -37,6 +44,9 @@ class Home : AppCompatActivity() {
                         changeFragment(frag_mission)
                     }
                     R.id.fourth -> {
+                        changeFragment(frag_tip)
+                    }
+                    R.id.fifth -> {
                         changeFragment(frag_history)
                     }
                 }
