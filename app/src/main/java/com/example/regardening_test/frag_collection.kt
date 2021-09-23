ffeb38beae967ba1d70931bb2208da2d8100ab16
YRim99 <alsrod2017@naver.com>
2021-09-24 01:10:49 +0900

    녜림

--- app/src/main/java/com/example/regardening_test/frag_collection.kt
+++ app/src/main/java/com/example/regardening_test/frag_collection.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.regardening_test.databinding.FragmentFragCollectionBinding
 
 class frag_collection : Fragment() {
+    //뷰 바인딩. xml에서 id 불러오기 위해 사용
+    private lateinit var binding : FragmentFragCollectionBinding
+    lateinit var mymoney : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
-
     ): View? {
+        mymoney = 1000
+        //1. 뷰 바인딩 설정
+        binding =  FragmentFragCollectionBinding.inflate(inflater, container, false)
+        //2. 바인딩으로 TextView 등에 접근
+        binding.totalMoney.text = mymoney
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_frag_collection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
